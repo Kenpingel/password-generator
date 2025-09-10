2614e6cbaa11724b14d9b97d231b8f786785a907
Kenpingel <pingel.kenneth@gmail.com>
2025-09-10 02:41:25 -0600

    refactor: rename functions and file names for clarity

--- Passwords.py
+++ Passwords.py
@@ -2,7 +2,7 @@ import secrets
 import string
 
 
-def passwords(length=12):
+def generate_passwords(length=12):
     # default length is 12 for the password.
 
     # choices is the string containing all characters that are acceptable for use in the password generation.
@@ -10,11 +10,11 @@ def passwords(length=12):
 
     # returns a string of characters at random from the string of possible characters with a default amount of one lowercase letter, one uppercase letter, one number, at at least one symbol.
     while True: 
-        password = "".join((secrets.choice(choices)) for i in range(length))
+        password = "".join(secrets.choice(choices) for i in range(length))
         if (any(ch.islower() for ch in password) and any(ch.isupper() for ch in password) and any(ch.isdigit() for ch in password) and any(ch in string.punctuation for ch in password)):
             break
     return password
 
 
 if __name__ == "__main__":
-    print(passwords())
+    print(generate_passwords())
